542825280bb49d2357a27e9504cac6e2c24cb3ec
Carlos Humberto Brasileiro Toscano <humbertobtoscano@gmail.com>
2024-09-05 15:46:37 -0300

    Primeiro upload

---
+++ app.js
@@ -0,0 +1,41 @@
+let resultadoHeader = document.getElementById("resultado-header");
+let composicaoItem = document.getElementById("composicao-item");
+let exibeAlbum = document.getElementById("exibe-album");
+
+function adicionarTag(texto, destino) {
+  let div = document.getElementById(destino);
+  div.appendChild(novaTag);
+}
+
+resultadoHeader.innerHTML = `
+  <div class="resultado-header">
+    <h2>${banda}</h2>
+    <p>${historia}</p> 
+  </div>
+`;
+
+composicaoItem.innerHTML = '';
+
+componentes.forEach(componente => {
+  composicaoItem.innerHTML += `
+    <div class="composicao-item">
+      <h5>${componente.nome}:</h5> 
+      <p>${componente.instrumento}</p>
+    </div>
+  `;
+});
+
+exibeAlbum.innerHTML = '';
+
+dados.forEach(dado => {
+  exibeAlbum.innerHTML += `
+    <div class="exibe-album">
+      <img src="${dado.cover}" alt="Cover do album ${dado.titulo}">
+      <div style="width: 40vw">
+        <h4>${dado.titulo}</h4>
+        <p>${dado.descricao}.</p>
+        <a href="${dado.link}" target="_blank">Saiba mais...</a>
+      </div>
+    </div>
+  `;
+});
---
+++ dados.js
@@ -0,0 +1,115 @@
+let banda = "Queen";
+
+let dados = [
+    {
+        titulo: "Queen (1973)",
+        descricao: "O álbum de estreia que apresentou ao mundo o som único da banda, com clássicos como 'Keep Yourself Alive' e 'Liar'",
+        link: "https://en.wikipedia.org/wiki/Queen_(Queen_album)",
+        cover: "https://upload.wikimedia.org/wikipedia/en/0/03/Queen_Queen.png?20111118001534"
+    },
+    {
+        titulo: "Queen II (1974)",
+        descricao: "Um álbum conceitual dividido em duas partes, com músicas mais experimentais e complexas",
+        link: "https://en.wikipedia.org/wiki/Queen_II",
+        cover: "https://upload.wikimedia.org/wikipedia/en/4/43/Queen_II_%28album_cover%29.jpg"
+    },
+    {
+        titulo: "Sheer Heart Attack (1974)",
+        descricao: "Um dos álbuns mais populares do Queen, com hits como 'Killer Queen' e 'Stone Cold Crazy'",
+        link: "https://en.wikipedia.org/wiki/Sheer_Heart_Attack",
+        cover: "https://upload.wikimedia.org/wikipedia/en/f/f4/Queen_Sheer_Heart_Attack.png"
+    },
+    {
+        titulo: "A Night at the Opera (1975)",
+        descricao: "Considerado por muitos como a obra-prima do Queen, com a épica 'Bohemian Rhapsody'",
+        link: "https://en.wikipedia.org/wiki/A_Night_at_the_Opera_(Queen_album)",
+        cover: "https://upload.wikimedia.org/wikipedia/en/4/4d/Queen_A_Night_At_The_Opera.png"
+    },
+    {
+        titulo: "A Day at the Races (1976):",
+        descricao: "A continuação de 'A Night at the Opera', com mais hits como 'Somebody to Love' e 'Tie Your Mother Down'",
+        link: "https://en.wikipedia.org/wiki/A_Day_at_the_Races_(album)",
+        cover: "https://upload.wikimedia.org/wikipedia/en/3/31/A_Day_at_the_Races_%28Queen%29.jpg"
+    },
+    {
+        titulo: "News of the World (1977):",
+        descricao: "Um dos álbuns mais vendidos do Queen, com os hinos 'We Are the Champions' e 'We Will Rock You'",
+        link: "https://en.wikipedia.org/wiki/News_of_the_World_(album)",
+        cover: "https://upload.wikimedia.org/wikipedia/en/e/ea/Queen_News_Of_The_World.png"
+    },
+    {
+        titulo: "Jazz (1978):",
+        descricao: "Um álbum mais experimental, com influências do jazz e da música disco",
+        link: "https://en.wikipedia.org/wiki/Jazz_(Queen_album)",
+        cover: "https://upload.wikimedia.org/wikipedia/en/0/06/Queen_Jazz.png"
+    },
+    {
+        titulo: "The Game (1980):",
+        descricao: "Um dos álbuns mais bem-sucedidos comercialmente do Queen, com hits como 'Crazy Little Thing Called Love' e 'Another One Bites the Dust'",
+        link: "https://en.wikipedia.org/wiki/The_Game_(Queen_album)",
+        cover: "https://upload.wikimedia.org/wikipedia/en/1/16/Queen_The_Game.png"
+    },
+    {
+        titulo: "Hot Space (1982):",
+        descricao: "Um álbum mais pop e dançante, com influências da música eletrônica",
+        link: "https://en.wikipedia.org/wiki/Hot_Space",
+        cover: "https://upload.wikimedia.org/wikipedia/en/3/3c/Queen_Hot_Space.png"
+    },
+    {
+        titulo: "The Works (1984):",
+        descricao: "Um retorno ao rock mais tradicional, com hits como 'Radio Ga Ga' e 'Hammer to Fall'",
+        link: "https://en.wikipedia.org/wiki/The_Works_(Queen_album)",
+        cover: "https://upload.wikimedia.org/wikipedia/en/3/39/Queen_The_Works.png"
+    },
+    {
+        titulo: "A Kind of Magic (1986):",
+        descricao: "A trilha sonora do filme Highlander, com a clássica 'Who Wants to Live Forever'",
+        link: "https://en.wikipedia.org/wiki/A_Kind_of_Magic",
+        cover: "https://upload.wikimedia.org/wikipedia/en/6/63/Queen_A_Kind_Of_Magic.png"
+    },
+    {
+        titulo: "The Miracle (1989):",
+        descricao: "Um álbum mais introspectivo, com músicas como 'I Want It All' e 'The Miracle'",
+        link: "https://en.wikipedia.org/wiki/The_Miracle_(album)",
+        cover: "https://upload.wikimedia.org/wikipedia/en/e/e3/Queen_The_Miracle.png"
+    },
+    {
+        titulo: "Innuendo (1991):",
+        descricao: "O último álbum de estúdio lançado em vida por Freddie Mercury, com a emocionante 'These Are the Days of Our Lives'",
+        link: "https://en.wikipedia.org/wiki/Innuendo_(album)",
+        cover: "https://upload.wikimedia.org/wikipedia/en/f/f7/Queen_Innuendo.png"
+    },
+    {
+        titulo: "Made in Heaven (1995):",
+        descricao: "Um álbum póstumo com músicas gravadas por Freddie Mercury antes de sua morte",
+        link: "https://en.wikipedia.org/wiki/Made_in_Heaven",
+        cover: "https://upload.wikimedia.org/wikipedia/en/0/03/Madeinheaven.jpg"
+    },
+];
+
+let historia = `O Queen é uma das bandas de rock mais icônicas e influentes de todos os tempos.  
+                Formada em Londres nos anos 70, a banda conquistou o mundo com sua música
+                extravagante, operística e poderosa. Com hits como "Bohemian Rhapsody", "We Will
+                Rock You" e "Don't Stop Me Now", o Queen se tornou sinônimo de rock de arena e
+                performances ao vivo eletrizantes. A voz inconfundível de Freddie Mercury,
+                aliada à virtuose musical de Brian May, Roger Taylor e John Deacon, criou um som
+                único que continua a inspirar gerações de músicos.`;
+
+let componentes = [
+    {
+        nome: "Freddie Mercury",
+        instrumento: "Vocais, Piano"
+    },
+    {
+        nome: "Brian May",
+        instrumento: "Guitarra, Vocais de apoio"
+    },
+    {
+        nome: "Roger Taylor",
+        instrumento: "Bateria, Vocais de apoio"
+    },
+    {
+        nome: "John Deacon",
+        instrumento: "Baixo"
+    },
+];
