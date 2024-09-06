27e32d2b5f918932f69257bf7ffce7d0a371a9c0
Carlos Humberto Brasileiro Toscano <humbertobtoscano@gmail.com>
2024-09-06 11:33:29 -0300

    Finalizando o projeto

--- app.js
+++ app.js
@@ -1,36 +1,60 @@
-let resultadoHeader = document.getElementById("resultado-header");
-let composicaoItem = document.getElementById("composicao-item");
-let exibeAlbum = document.getElementById("exibe-album");
+function pesquisar() {
 
-resultadoHeader.innerHTML = `
-  <div class="resultado-header">
-    <h2>${banda}</h2>
-    <p>${historia}</p> 
-  </div>
-`;
+  let idBanda = -1;
+  let resultadoHeader = document.getElementById("resultado-header");
+  let composicaoItem = document.getElementById("composicao-item");
+  let exibeAlbum = document.getElementById("exibe-album");
 
-composicaoItem.innerHTML = '';
+  const campoPesquisa = document.getElementById("campo-pesquisa");
 
-componentes.forEach(componente => {
-  composicaoItem.innerHTML += `
-    <div class="composicao-item">
-      <h5>${componente.nome}:</h5> 
-      <p>${componente.instrumento}</p>
+  let busca = campoPesquisa.value;
+  
+  dados.forEach(item => {
+    
+    let banda = item.banda.toUpperCase();
+    
+    if (banda.includes(busca.toUpperCase()) && busca != '') {
+      idBanda = item.id;
+    }
+    
+  });
+ 
+  if (idBanda == -1) {
+    alert('Esta é uma versão Demo, banco de dados contém dados do Queen e do Pink Floyd');
+    return;
+  }
+  
+  resultadoHeader.innerHTML = `
+    <div class="resultado-header">
+      <h2>${dados[idBanda].banda}</h2>
+      <p>${dados[idBanda].historia}</p> 
     </div>
   `;
-});
-
-exibeAlbum.innerHTML = '';
-
-dados.forEach(dado => {
-  exibeAlbum.innerHTML += `
-    <div class="exibe-album">
-      <img src="${dado.cover}" alt="Cover do album ${dado.titulo}">
-      <div style="width: 40vw">
-        <h4>${dado.titulo}</h4>
-        <p>${dado.descricao}.</p>
-        <a href="${dado.link}" target="_blank">Saiba mais...</a>
+  
+  composicaoItem.innerHTML = '<h4>Composição da Banda</h4>';
+  
+  dados[idBanda].componentes.forEach(componente => {
+    composicaoItem.innerHTML += `
+      <div class="composicao-item">
+        <h5>${componente.nome}:</h5> 
+        <p>${componente.instrumento}</p>
       </div>
-    </div>
-  `;
-});
+    `;
+  });
+  
+  exibeAlbum.innerHTML = '<h4>Álbuns de Estúdio</h4>';
+  
+  dados[idBanda].discografia.forEach(dado => {
+    exibeAlbum.innerHTML += `
+      <div class="exibe-album">
+        <img src="${dado.cover}" alt="Cover do album ${dado.titulo}">
+        <div style="width: 40vw">
+          <h4>${dado.titulo}</h4>
+          <p>${dado.descricao}.</p>
+          <a href="${dado.link}" target="_blank">Saiba mais...</a>
+        </div>
+      </div>
+    `;
+  });
+
+}
--- dados.js
+++ dados.js
@@ -1,115 +1,214 @@
-let banda = "Queen";
-
 let dados = [
     {
-        titulo: "Queen (1973)",
-        descricao: "O álbum de estreia que apresentou ao mundo o som único da banda, com clássicos como 'Keep Yourself Alive' e 'Liar'",
-        link: "https://en.wikipedia.org/wiki/Queen_(Queen_album)",
-        cover: "https://upload.wikimedia.org/wikipedia/en/0/03/Queen_Queen.png?20111118001534"
+        id: 0,
+        banda: "Queen",
+        discografia: [
+            {
+                titulo: "Queen (1973)",
+                descricao: "O álbum de estreia que apresentou ao mundo o som único da banda, com clássicos como 'Keep Yourself Alive' e 'Liar'",
+                link: "https://en.wikipedia.org/wiki/Queen_(Queen_album)",
+                cover: "https://upload.wikimedia.org/wikipedia/en/0/03/Queen_Queen.png?20111118001534"
+            },
+            {
+                titulo: "Queen II (1974)",
+                descricao: "Um álbum conceitual dividido em duas partes, com músicas mais experimentais e complexas",
+                link: "https://en.wikipedia.org/wiki/Queen_II",
+                cover: "https://upload.wikimedia.org/wikipedia/en/4/43/Queen_II_%28album_cover%29.jpg"
+            },
+            {
+                titulo: "Sheer Heart Attack (1974)",
+                descricao: "Um dos álbuns mais populares do Queen, com hits como 'Killer Queen' e 'Stone Cold Crazy'",
+                link: "https://en.wikipedia.org/wiki/Sheer_Heart_Attack",
+                cover: "https://upload.wikimedia.org/wikipedia/en/f/f4/Queen_Sheer_Heart_Attack.png"
+            },
+            {
+                titulo: "A Night at the Opera (1975)",
+                descricao: "Considerado por muitos como a obra-prima do Queen, com a épica 'Bohemian Rhapsody'",
+                link: "https://en.wikipedia.org/wiki/A_Night_at_the_Opera_(Queen_album)",
+                cover: "https://upload.wikimedia.org/wikipedia/en/4/4d/Queen_A_Night_At_The_Opera.png"
+            },
+            {
+                titulo: "A Day at the Races (1976):",
+                descricao: "A continuação de 'A Night at the Opera', com mais hits como 'Somebody to Love' e 'Tie Your Mother Down'",
+                link: "https://en.wikipedia.org/wiki/A_Day_at_the_Races_(album)",
+                cover: "https://upload.wikimedia.org/wikipedia/en/3/31/A_Day_at_the_Races_%28Queen%29.jpg"
+            },
+            {
+                titulo: "News of the World (1977):",
+                descricao: "Um dos álbuns mais vendidos do Queen, com os hinos 'We Are the Champions' e 'We Will Rock You'",
+                link: "https://en.wikipedia.org/wiki/News_of_the_World_(album)",
+                cover: "https://upload.wikimedia.org/wikipedia/en/e/ea/Queen_News_Of_The_World.png"
+            },
+            {
+                titulo: "Jazz (1978):",
+                descricao: "Um álbum mais experimental, com influências do jazz e da música disco",
+                link: "https://en.wikipedia.org/wiki/Jazz_(Queen_album)",
+                cover: "https://upload.wikimedia.org/wikipedia/en/0/06/Queen_Jazz.png"
+            },
+            {
+                titulo: "The Game (1980):",
+                descricao: "Um dos álbuns mais bem-sucedidos comercialmente do Queen, com hits como 'Crazy Little Thing Called Love' e 'Another One Bites the Dust'",
+                link: "https://en.wikipedia.org/wiki/The_Game_(Queen_album)",
+                cover: "https://upload.wikimedia.org/wikipedia/en/1/16/Queen_The_Game.png"
+            },
+            {
+                titulo: "Hot Space (1982):",
+                descricao: "Um álbum mais pop e dançante, com influências da música eletrônica",
+                link: "https://en.wikipedia.org/wiki/Hot_Space",
+                cover: "https://upload.wikimedia.org/wikipedia/en/3/3c/Queen_Hot_Space.png"
+            },
+            {
+                titulo: "The Works (1984):",
+                descricao: "Um retorno ao rock mais tradicional, com hits como 'Radio Ga Ga' e 'Hammer to Fall'",
+                link: "https://en.wikipedia.org/wiki/The_Works_(Queen_album)",
+                cover: "https://upload.wikimedia.org/wikipedia/en/3/39/Queen_The_Works.png"
+            },
+            {
+                titulo: "A Kind of Magic (1986):",
+                descricao: "A trilha sonora do filme Highlander, com a clássica 'Who Wants to Live Forever'",
+                link: "https://en.wikipedia.org/wiki/A_Kind_of_Magic",
+                cover: "https://upload.wikimedia.org/wikipedia/en/6/63/Queen_A_Kind_Of_Magic.png"
+            },
+            {
+                titulo: "The Miracle (1989):",
+                descricao: "Um álbum mais introspectivo, com músicas como 'I Want It All' e 'The Miracle'",
+                link: "https://en.wikipedia.org/wiki/The_Miracle_(album)",
+                cover: "https://upload.wikimedia.org/wikipedia/en/e/e3/Queen_The_Miracle.png"
+            },
+            {
+                titulo: "Innuendo (1991):",
+                descricao: "O último álbum de estúdio lançado em vida por Freddie Mercury, com a emocionante 'These Are the Days of Our Lives'",
+                link: "https://en.wikipedia.org/wiki/Innuendo_(album)",
+                cover: "https://upload.wikimedia.org/wikipedia/en/f/f7/Queen_Innuendo.png"
+            },
+            {
+                titulo: "Made in Heaven (1995):",
+                descricao: "Um álbum póstumo com músicas gravadas por Freddie Mercury antes de sua morte",
+                link: "https://en.wikipedia.org/wiki/Made_in_Heaven",
+                cover: "https://upload.wikimedia.org/wikipedia/en/0/03/Madeinheaven.jpg"
+            },    
+        ],
+        historia: `O Queen é uma das bandas de rock mais icônicas e influentes de todos os tempos.  
+                    Formada em Londres nos anos 70, a banda conquistou o mundo com sua música
+                    extravagante, operística e poderosa. Com hits como "Bohemian Rhapsody", "We Will
+                    Rock You" e "Don't Stop Me Now", o Queen se tornou sinônimo de rock de arena e
+                    performances ao vivo eletrizantes. A voz inconfundível de Freddie Mercury,
+                    aliada à virtuose musical de Brian May, Roger Taylor e John Deacon, criou um som
+                    único que continua a inspirar gerações de músicos.`,
+        componentes: [
+            {
+                nome: "Freddie Mercury",
+                instrumento: "Vocais, Piano"
+            },
+            {
+                nome: "Brian May",
+                instrumento: "Guitarra, Vocais de apoio"
+            },
+            {
+                nome: "Roger Taylor",
+                instrumento: "Bateria, Vocais de apoio"
+            },
+            {
+                nome: "John Deacon",
+                instrumento: "Baixo"
+            },    
+        ]    
     },
     {
-        titulo: "Queen II (1974)",
-        descricao: "Um álbum conceitual dividido em duas partes, com músicas mais experimentais e complexas",
-        link: "https://en.wikipedia.org/wiki/Queen_II",
-        cover: "https://upload.wikimedia.org/wikipedia/en/4/43/Queen_II_%28album_cover%29.jpg"
-    },
-    {
-        titulo: "Sheer Heart Attack (1974)",
-        descricao: "Um dos álbuns mais populares do Queen, com hits como 'Killer Queen' e 'Stone Cold Crazy'",
-        link: "https://en.wikipedia.org/wiki/Sheer_Heart_Attack",
-        cover: "https://upload.wikimedia.org/wikipedia/en/f/f4/Queen_Sheer_Heart_Attack.png"
-    },
-    {
-        titulo: "A Night at the Opera (1975)",
-        descricao: "Considerado por muitos como a obra-prima do Queen, com a épica 'Bohemian Rhapsody'",
-        link: "https://en.wikipedia.org/wiki/A_Night_at_the_Opera_(Queen_album)",
-        cover: "https://upload.wikimedia.org/wikipedia/en/4/4d/Queen_A_Night_At_The_Opera.png"
-    },
-    {
-        titulo: "A Day at the Races (1976):",
-        descricao: "A continuação de 'A Night at the Opera', com mais hits como 'Somebody to Love' e 'Tie Your Mother Down'",
-        link: "https://en.wikipedia.org/wiki/A_Day_at_the_Races_(album)",
-        cover: "https://upload.wikimedia.org/wikipedia/en/3/31/A_Day_at_the_Races_%28Queen%29.jpg"
-    },
-    {
-        titulo: "News of the World (1977):",
-        descricao: "Um dos álbuns mais vendidos do Queen, com os hinos 'We Are the Champions' e 'We Will Rock You'",
-        link: "https://en.wikipedia.org/wiki/News_of_the_World_(album)",
-        cover: "https://upload.wikimedia.org/wikipedia/en/e/ea/Queen_News_Of_The_World.png"
-    },
-    {
-        titulo: "Jazz (1978):",
-        descricao: "Um álbum mais experimental, com influências do jazz e da música disco",
-        link: "https://en.wikipedia.org/wiki/Jazz_(Queen_album)",
-        cover: "https://upload.wikimedia.org/wikipedia/en/0/06/Queen_Jazz.png"
-    },
-    {
-        titulo: "The Game (1980):",
-        descricao: "Um dos álbuns mais bem-sucedidos comercialmente do Queen, com hits como 'Crazy Little Thing Called Love' e 'Another One Bites the Dust'",
-        link: "https://en.wikipedia.org/wiki/The_Game_(Queen_album)",
-        cover: "https://upload.wikimedia.org/wikipedia/en/1/16/Queen_The_Game.png"
-    },
-    {
-        titulo: "Hot Space (1982):",
-        descricao: "Um álbum mais pop e dançante, com influências da música eletrônica",
-        link: "https://en.wikipedia.org/wiki/Hot_Space",
-        cover: "https://upload.wikimedia.org/wikipedia/en/3/3c/Queen_Hot_Space.png"
-    },
-    {
-        titulo: "The Works (1984):",
-        descricao: "Um retorno ao rock mais tradicional, com hits como 'Radio Ga Ga' e 'Hammer to Fall'",
-        link: "https://en.wikipedia.org/wiki/The_Works_(Queen_album)",
-        cover: "https://upload.wikimedia.org/wikipedia/en/3/39/Queen_The_Works.png"
-    },
-    {
-        titulo: "A Kind of Magic (1986):",
-        descricao: "A trilha sonora do filme Highlander, com a clássica 'Who Wants to Live Forever'",
-        link: "https://en.wikipedia.org/wiki/A_Kind_of_Magic",
-        cover: "https://upload.wikimedia.org/wikipedia/en/6/63/Queen_A_Kind_Of_Magic.png"
-    },
-    {
-        titulo: "The Miracle (1989):",
-        descricao: "Um álbum mais introspectivo, com músicas como 'I Want It All' e 'The Miracle'",
-        link: "https://en.wikipedia.org/wiki/The_Miracle_(album)",
-        cover: "https://upload.wikimedia.org/wikipedia/en/e/e3/Queen_The_Miracle.png"
-    },
-    {
-        titulo: "Innuendo (1991):",
-        descricao: "O último álbum de estúdio lançado em vida por Freddie Mercury, com a emocionante 'These Are the Days of Our Lives'",
-        link: "https://en.wikipedia.org/wiki/Innuendo_(album)",
-        cover: "https://upload.wikimedia.org/wikipedia/en/f/f7/Queen_Innuendo.png"
-    },
-    {
-        titulo: "Made in Heaven (1995):",
-        descricao: "Um álbum póstumo com músicas gravadas por Freddie Mercury antes de sua morte",
-        link: "https://en.wikipedia.org/wiki/Made_in_Heaven",
-        cover: "https://upload.wikimedia.org/wikipedia/en/0/03/Madeinheaven.jpg"
-    },
-];
-
-let historia = `O Queen é uma das bandas de rock mais icônicas e influentes de todos os tempos.  
-                Formada em Londres nos anos 70, a banda conquistou o mundo com sua música
-                extravagante, operística e poderosa. Com hits como "Bohemian Rhapsody", "We Will
-                Rock You" e "Don't Stop Me Now", o Queen se tornou sinônimo de rock de arena e
-                performances ao vivo eletrizantes. A voz inconfundível de Freddie Mercury,
-                aliada à virtuose musical de Brian May, Roger Taylor e John Deacon, criou um som
-                único que continua a inspirar gerações de músicos.`;
-
-let componentes = [
-    {
-        nome: "Freddie Mercury",
-        instrumento: "Vocais, Piano"
-    },
-    {
-        nome: "Brian May",
-        instrumento: "Guitarra, Vocais de apoio"
-    },
-    {
-        nome: "Roger Taylor",
-        instrumento: "Bateria, Vocais de apoio"
-    },
-    {
-        nome: "John Deacon",
-        instrumento: "Baixo"
+        id: 1,
+        banda: "Pink Floyd",
+        discografia: [
+            {
+                titulo: "The Piper at the Gates of Dawn (1967)",
+                descricao: "Álbum de estreia, marcado pela psicdelia e pela genialidade de Syd Barrett. Clássicos como 'Arnold Layne' e 'See Emily Play' marcaram época",
+                link: "https://en.wikipedia.org/wiki/The_Piper_at_the_Gates_of_Dawn",
+                cover: "https://upload.wikimedia.org/wikipedia/en/3/3c/PinkFloyd-album-piperatthegatesofdawn_300.jpg"
+            },
+            {
+                titulo: "A Saucerful of Secrets (1968)",
+                descricao: "Último álbum com Syd Barrett e primeiro com David Gilmour. Apresenta um som mais pesado e experimental",
+                link: "https://en.wikipedia.org/wiki/A_Saucerful_of_Secrets",
+                cover: "https://upload.wikimedia.org/wikipedia/en/5/54/Saucerful_of_secrets2.jpg"
+            },
+            {
+                titulo: "The Dark Side of the Moon (1973)",
+                descricao: "Um dos álbuns mais vendidos da história da música, com temas como tempo, loucura e a condição humana. Clássicos como 'Time' e 'Money'",
+                link: "https://en.wikipedia.org/wiki/The_Dark_Side_of_the_Moon",
+                cover: "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png"
+            },
+            {
+                titulo: "Wish You Were Here (1975)",
+                descricao: "Álbum conceitual que aborda temas como a fama, a solidão e a perda. A icônica capa retrata dois homens de negócios apertando as mãos, enquanto um deles está em chamas.",
+                link: "https://en.wikipedia.org/wiki/Wish_You_Were_Here_(Pink_Floyd_album)",
+                cover: "https://upload.wikimedia.org/wikipedia/en/a/a4/Pink_Floyd%2C_Wish_You_Were_Here_%281975%29.png"
+            },
+            {
+                titulo: "Animals (1977)",
+                descricao: "Álbum conceitual que satiriza a sociedade, dividindo as pessoas em três grupos: cães, porcos e ovelhas",
+                link: "https://en.wikipedia.org/wiki/Animals_(Pink_Floyd_album)",
+                cover: "https://upload.wikimedia.org/wikipedia/en/7/74/Pink_Floyd-Animals-Frontal.jpg"
+            },
+            {
+                titulo: "The Wall (1979)",
+                descricao: "Um dos álbuns mais ambiciosos e controversos do Pink Floyd, que narra a história de um rock star que se isola do mundo",
+                link: "https://en.wikipedia.org/wiki/The_Wall",
+                cover: "https://pure-music.co.uk/wp-content/uploads/2019/11/the-wall-album-cover.jpg"
+            },
+            {
+                titulo: "A Momentary Lapse of Reason (1987)",
+                descricao: "Primeiro álbum do Pink Floyd após a saída de Roger Waters. Apresenta um som mais acessível, com sucessos como 'Learning to Fly'",
+                link: "https://en.wikipedia.org/wiki/A_Momentary_Lapse_of_Reason",
+                cover: "https://upload.wikimedia.org/wikipedia/en/4/4e/MLoRLP01.jpg"
+            },
+            {
+                titulo: "The Division Bell (1994)",
+                descricao: "Continuação do trabalho iniciado em 'A Momentary Lapse of Reason', com temas como a comunicação e a passagem do tempo",
+                link: "https://en.wikipedia.org/wiki/The_Division_Bell",
+                cover: "https://upload.wikimedia.org/wikipedia/en/6/6e/Pink_Floyd_-_Division_Bell.jpg"
+            },
+        ],
+        historia: `O Pink Floyd, uma das bandas mais influentes e inovadoras da história do rock, surgiu em Londres no ano de 1965. 
+                   Fundada por Syd Barrett (guitarra, vocal), Nick Mason (bateria), Roger Waters (baixo, voz) e Richard Wright 
+                   (teclados, voz), a banda inicialmente se aventurava por um som psicodélico e experimental, que os tornaria um dos 
+                   expoentes do movimento underground da época.<br><br>
+                   <strong>Os Primeiros Passos e o Brilho de Syd Barrett</strong><br><br>
+                   Com álbuns como "The Piper at the Gates of Dawn" (1967), o Pink Floyd conquistou um público fiel com suas letras 
+                   surrealistas e melodias hipnotizantes. A genialidade de Syd Barrett era inegável, mas seus problemas mentais, 
+                   agravados pelo uso de drogas, o afastaram da banda em 1968.<br><br>
+                   <strong>Uma Nova Era e o Sucesso Comercial</strong><br><br>
+                   Com a saída de Barrett, David Gilmour ingressou no grupo, trazendo uma nova dinâmica e um som mais acessível. 
+                   A partir daí, o Pink Floyd começou a explorar sonoridades mais progressivas e conceituais, com álbuns como 
+                   "The Dark Side of the Moon" (1973), um dos discos mais vendidos da história e um marco do rock progressivo.<br><br>
+                   <strong>A Era Clássica e a Dissolução</strong><br><br>
+                   Ao longo dos anos 70 e início dos 80, o Pink Floyd consolidou seu status de lenda, com álbuns como "Wish You Were Here", 
+                   "Animals" e "The Wall". No entanto, as tensões internas, especialmente entre Roger Waters e os demais membros, levaram 
+                   à dissolução da banda em 1985.<br><br>
+                   <strong>Legado e Reconhecimento</strong><br><br>
+                   Apesar da separação, o Pink Floyd deixou um legado incontestável. Suas músicas, marcadas por letras profundas, 
+                   experimentações sonoras e performances visuais inovadoras, continuam a encantar gerações. A banda é reconhecida 
+                   mundialmente por sua capacidade de criar atmosferas únicas e de explorar temas complexos como a alienação, 
+                   a solidão e a passagem do tempo.`,
+        componentes: [
+            {
+                nome: "Syd Barrett",
+                instrumento: " Guitarra, vocais"
+            },
+            {
+                nome: "Roger Waters",
+                instrumento: "Baixo, vocais"
+            },
+            {
+                nome: "Nick Mason",
+                instrumento: "Bateria"
+            },
+            {
+                nome: "Richard Wright",
+                instrumento: "Teclados, vocais"
+            },    
+            {
+                nome: "David Gilmour",
+                instrumento: "Guitarra, vocais"
+            }
+        ]    
     },
 ];
